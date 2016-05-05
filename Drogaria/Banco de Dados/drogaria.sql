6e11db19d864b201475d976f8cc87b765205c202
Felipe Muniz <mrmunz2012@gmail.com>
2016-05-05 00:02:45 -0300

    Bando de dados Enviado.

---
+++ Drogaria/Banco de Dados/drogaria.sql
@@ -0,0 +1,67 @@
+CREATE TABLE `funcionario` (
+`id` int(11) NOT NULL,
+`nome` varchar(255) NULL,
+`salario` decimal NULL,
+`cpf` varchar(25) NULL,
+`endereco` varchar(255) NULL,
+`telefone` varchar(255) NULL,
+`comicao` decimal NULL,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `cliente` (
+`id` int(11) NOT NULL,
+`nome` varchar(255) NULL,
+`telefone` varchar(15) NULL,
+`cpf` varchar(25) NULL,
+`endereco` text NULL,
+`funcionario` int(11) NULL,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `produto` (
+`id` int(11) NOT NULL,
+`nome` varchar(255) NULL,
+`valor` decimal NULL,
+`fabricante` int(11) NULL,
+`fornecedor` int(11) NULL,
+`funcionario` int(11) NULL,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `pedido` (
+`id` int(11) NOT NULL,
+`total` decimal NULL,
+`cliente` int(11) NULL,
+`funcionario` int(11) NULL,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `fabricante` (
+`id` int(11) NOT NULL,
+`nome` varchar(255) NULL,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `fornecedor` (
+`id` int(11) NOT NULL,
+`nome` varchar(255) NULL,
+PRIMARY KEY (`id`) 
+);
+
+CREATE TABLE `pedido_produto` (
+`id` int(11) NOT NULL AUTO_INCREMENT,
+`id_pedido` int(11) NULL,
+`id_produto` int(11) NULL,
+PRIMARY KEY (`id`) 
+);
+
+
+ALTER TABLE `pedido` ADD CONSTRAINT `fk_lista_cliente` FOREIGN KEY (`cliente`) REFERENCES `cliente` (`id`);
+ALTER TABLE `pedido` ADD CONSTRAINT `fk_lista_funcionario` FOREIGN KEY (`funcionario`) REFERENCES `funcionario` (`id`);
+ALTER TABLE `produto` ADD CONSTRAINT `fk_produto_fornecedor` FOREIGN KEY (`fornecedor`) REFERENCES `fornecedor` (`id`);
+ALTER TABLE `produto` ADD CONSTRAINT `fk_produto_fabricante` FOREIGN KEY (`fabricante`) REFERENCES `fabricante` (`id`);
+ALTER TABLE `produto` ADD CONSTRAINT `fk_produto_funcionario` FOREIGN KEY (`funcionario`) REFERENCES `funcionario` (`id`);
+ALTER TABLE `pedido_produto` ADD CONSTRAINT `pp_pedido` FOREIGN KEY (`id_pedido`) REFERENCES `pedido` (`id`);
+ALTER TABLE `pedido_produto` ADD CONSTRAINT `pp_produto` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id`);
+
--- Drogaria/src/view/Main.java
+++ Drogaria/src/view/Main.java
@@ -25,17 +25,27 @@ public class Main extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("jLabel1");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addContainerGap(356, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(23, 23, 23)
+                .addComponent(jLabel1)
+                .addContainerGap(263, Short.MAX_VALUE))
         );
 
         pack();
@@ -77,5 +87,6 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
